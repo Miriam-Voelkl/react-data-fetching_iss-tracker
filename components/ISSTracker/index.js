b9c5df14c741c005a4501a8aa72539487ac97d94
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-19 11:20:18 +0100

    add swr fetching

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,11 +1,26 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
+  const {
+    data: issPosition,
+    error,
+    isLoading,
+  } = useSWR(URL, fetcher, { refreshInterval: 5000 });
+
+  if (isLoading) {
+    return <h1>...loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>ISS position cannot be found: {error.message}</h1>;
+  }
+
+  /* const [coords, setCoords] = useState({
     longitude: 0,
     latitude: 0,
   });
@@ -30,15 +45,16 @@ export default function ISSTracker() {
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, []); 
+  */
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={issPosition.longitude} latitude={issPosition.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={issPosition.longitude}
+        latitude={issPosition.latitude}
+        // onRefresh={getISSCoords}
       />
     </main>
   );
